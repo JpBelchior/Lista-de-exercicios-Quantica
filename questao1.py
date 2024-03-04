e8833b1dfa47cf0f19b91eaae269b05af9f224e2
JpBelchior <joaopedrobelchior@hotmail.com>
2024-03-04 12:23:30 -0300

    tutu

--- questao1.py
+++ questao1.py
@@ -1,1 +1,10 @@
-print("hello world")
+#vamos analisar se a equação possui raizes reais. faça y=x**2.
+a=1
+b=2
+c=1
+y=(-b + (b**2-4*a*c)**(1/2))/2
+if y<0:
+    print("não há solução real")
+else:
+    print("há solução real")
+
